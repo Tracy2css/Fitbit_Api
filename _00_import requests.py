7273e861dd27685e4965a34925e41b05c237f3df
Tracy_D <tracy2css@gmail.com>
2024-08-07 07:10:52 +1000

    0807_Name_Update

--- _00_import requests.py
+++ _00_import requests.py
@@ -6,7 +6,7 @@ client_id = '23PDS3'
 client_secret = '8be183a112c011a53c188db507728577'  # Replace with your actual client secret
 
 # Authorization code received from Fitbit
-authorization_code = 'bf1801d6a095e32388a7d137e0f963e70727d512'
+authorization_code = '376ee9f215640c212768d90522767ec932427d8f'
 
 # Base64 encode the client ID and client secret
 credentials = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
@@ -23,7 +23,7 @@ data = {
     'grant_type': 'authorization_code',
     'redirect_uri': 'http://127.0.0.1:8080/',  # Replace with your actual redirect URI
     'code': authorization_code,
-    'code_verifier': '2s1x3y6m474i1x2v6j5u4f0i1e295m300e502v4a335s0m301r6s3c15494b580r5u2j6o6o4d141l0s1e37083i5s2j306x3r304w5h5i593e6g2n1s3x5y35702a0f'  # Replace with your actual code verifier
+    'code_verifier': '052p6s3o3t5g68663h1x6e0k372h111a38605d2h5y601m6o0a076j4f2g5u2u0x2c0h104x6m300u2f3k2p5a005h582g0q011p63533d5q2p5g566e0o160a5k5e5i'  # Replace with your actual code verifier
 }
 
 # Send POST request to Fitbit token endpoint
